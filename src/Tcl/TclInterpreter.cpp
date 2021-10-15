ed971f0375ec5538f3c17bf9506055b95cfeb7e9
Alain Dargelas <alain.dargelas@gmail.com>
2021-10-14 20:49:21 -0700

    Enable Google Tests for Tcl

---
+++ src/Tcl/TclInterpreter.cpp
@@ -0,0 +1,51 @@
+/*
+ Copyright 2021 The Foedag team
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#include "TclInterpreter.h"
+
+TclInterpreter::TclInterpreter(const char *argv0) : interp(nullptr) {
+  static bool initLib;
+  if (!initLib) {
+    Tcl_FindExecutable(argv0);
+    initLib = true;
+  }
+  interp = Tcl_CreateInterp();
+  if (!interp) throw new std::runtime_error("failed to initialise Tcl library");
+}
+
+TclInterpreter::~TclInterpreter() {
+  if (interp) Tcl_DeleteInterp(interp);
+}
+
+std::string TclInterpreter::evalFile(const std::string &filename) {
+  int code = Tcl_EvalFile(interp, filename.c_str());
+
+  if (code >= TCL_ERROR) {
+    return std::string("Tcl Error: " +
+                       std::string(Tcl_GetStringResult(interp)));
+  }
+  return std::string(Tcl_GetStringResult(interp));
+}
+
+std::string TclInterpreter::evalCmd(const std::string cmd) {
+  int code = Tcl_Eval(interp, cmd.c_str());
+
+  if (code >= TCL_ERROR) {
+    return std::string("Tcl Error: " +
+                       std::string(Tcl_GetStringResult(interp)));
+  }
+  return std::string(Tcl_GetStringResult(interp));
+}
