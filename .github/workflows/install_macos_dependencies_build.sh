e2b3f569d8fdbc4f21447645989fbb58c6c8bb25
tangxifan <tangxifan@gmail.com>
2021-10-22 13:47:31 -0700

    [CI] Bug fix

--- .github/workflows/install_macos_dependencies_build.sh
+++ .github/workflows/install_macos_dependencies_build.sh
@@ -1,4 +1,5 @@
 # Install required dependencies for Mac OS systems
 brew install qt5
-echo 'export PATH="/opt/homebrew/opt/qt@5/bin:$PATH"' >> ~/.zshrc
-source ~/.zshrc
+export PATH="/usr/local/opt/qt@5/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/qt@5/lib"
+export CPPFLAGS="-I/usr/local/opt/qt@5/include"
