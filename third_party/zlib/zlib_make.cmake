94ce0244ddb4f5d6d0c4253245bdce3449a9d56c
Alain Dargelas <alain.dargelas@gmail.com>
2021-11-05 20:10:45 -0700

    zlib

---
+++ third_party/zlib/zlib_make.cmake
@@ -0,0 +1,13 @@
+execute_process(
+    COMMAND ./configure  --prefix=${CMAKE_CURRENT_BINARY_DIR}
+    ERROR_QUIET
+)
+execute_process(
+    COMMAND make
+    ERROR_QUIET
+)
+execute_process(
+    COMMAND make install
+    ERROR_QUIET
+)
+
