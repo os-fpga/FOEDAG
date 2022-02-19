f60fd6e410d5e7518c21dac9f33351af869f097b
Alain Dargelas <alain.dargelas@gmail.com>
2022-02-18 18:25:59 -0800

    centos build in bash

---
+++ .github/workflows/install_centos_dependencies_build.sh
@@ -0,0 +1,39 @@
+# Install required dependencies for CentOS systems
+yum update -y
+yum group install -y "Development Tools" 
+yum install -y epel-release 
+curl -C - -O https://cmake.org/files/v3.15/cmake-3.15.7-Linux-x86_64.tar.gz
+tar xzf cmake-3.15.7-Linux-x86_64.tar.gz
+ln -s $PWD/cmake-3.15.7-Linux-x86_64/bin/cmake /usr/bin/cmake
+yum install -y centos-release-scl
+yum install -y devtoolset-9
+yum install -y devtoolset-9-toolchain
+yum install -y devtoolset-9-gcc-c++
+scl enable devtoolset-9 bash
+yum install -y tcl
+yum install -y make
+yum install -y swig
+yum install -y which
+yum install -y google-perftools
+yum install -y gperftools gperftools-devel
+yum install -y uuid-devel
+yum install -y valgrind
+yum install -y python3
+yum install -y xorg-x11-server-Xorg xorg-x11-xauth xorg-x11-apps 
+yum install -y xorg-x11-server-Xvfb
+yum install -y mesa-libGL-devel
+yum install -y libxcb libxcb-devel xcb-util xcb-util-devel libxkbcommon-devel libxkbcommon-x11-devel
+yum install -y xcb-util-image-devel xcb-util-keysyms-devel xcb-util-renderutil-devel xcb-util-wm-devel
+ln -s $PWD/cmake-3.15.7-Linux-x86_64/bin/ctest /usr/bin/ctest
+curl -L http://download.qt.io/official_releases/qt/5.15/5.15.0/single/qt-everywhere-src-5.15.0.tar.xz --output qt-everywhere-src-5.15.0.tar.xz
+tar -xf qt-everywhere-src-5.15.0.tar.xz
+mkdir buildqt5
+cd buildqt5
+source /opt/rh/devtoolset-9/enable
+echo 'QMAKE_CC=/opt/rh/devtoolset-9/root/usr/bin/gcc' >> $GITHUB_ENV
+echo 'QMAKE_CXX=/opt/rh/devtoolset-9/root/usr/bin/g++' >> $GITHUB_ENV
+echo 'PATH=/usr/local/Qt-5.15.0/bin:/usr/lib/ccache:'"$PATH" >> $GITHUB_ENV
+../qt-everywhere-src-5.15.0/configure -opensource -confirm-license -xcb -xcb-xlib -bundled-xcb-xinput
+make -j 2
+make install
+cd ..
