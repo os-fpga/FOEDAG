89014e877f77590febc4c1eb346cc704f271b869
Alain Dargelas <alain.dargelas@gmail.com>
2022-04-27 15:57:39 -0700

    Tcl standard non-interractive behavior

--- tests/TestGui/compiler_flow.tcl
+++ tests/TestGui/compiler_flow.tcl
@@ -1,6 +1,5 @@
 create_design BLAH
 set_top_module toto
-batch {
 ipgenerate
 synth
 packing
@@ -10,13 +9,4 @@ route
 sta
 power
 bitstream
-}
-
-
-after 22000 {set CONT 0}
-set CONT 1 
-while {$CONT} {
-  set a 0
-  after 100 set a 1
-  vwait a
-}
+exit
