df56398afa2ae703af62559956d3481febcca35f
Skyler-rs <skyler@rapidsilicon.com>
2022-05-16 09:03:45 -0700

    Fixing include paths

--- src/Main/WidgetFactory.h
+++ src/Main/WidgetFactory.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <QtWidgets>
 
-#include "nlohmann_json/json.hpp"
+#include "../third_party/nlohmann_json/json.hpp"
 using json = nlohmann::ordered_json;
 
 namespace FOEDAG {
