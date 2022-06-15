8828507408a60d818ef4b768fe5dc72233344815
Alain Dargelas <alain.dargelas@gmail.com>
2022-06-15 15:36:16 -0700

    skeleton code for ipgenerate

---
+++ src/IPGenerate/IPGenerator.cpp
@@ -0,0 +1,53 @@
+/*
+Copyright 2022 The Foedag team
+
+GPL License
+
+Copyright (c) 2022 The Open-Source FPGA Foundation
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifdef _WIN32
+#include <Windows.h>
+#include <direct.h>
+#include <process.h>
+#else
+#include <stdlib.h>
+#include <sys/param.h>
+#include <unistd.h>
+#endif
+
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <QDebug>
+#include <QProcess>
+#include <chrono>
+#include <ctime>
+#include <filesystem>
+#include <sstream>
+#include <thread>
+
+#include "Compiler/Log.h"
+#include "Compiler/ProcessUtils.h"
+#include "Compiler/TclInterpreterHandler.h"
+#include "Compiler/WorkerThread.h"
+#include "IPGenerate/IPGenerator.h"
+#include "MainWindow/Session.h"
+
+extern FOEDAG::Session* GlobalSession;
+using namespace FOEDAG;
+using Time = std::chrono::high_resolution_clock;
+using ms = std::chrono::milliseconds;
