7b162453f419ad68c51bf347a88ef4f328fbec1f
KochynVolodymyr <95262932+KochynVolodymyr@users.noreply.github.com>
2022-06-17 21:50:43 +0300

    Small refactoring (#470)

--- src/Compiler/WorkerThread.h
+++ src/Compiler/WorkerThread.h
@@ -21,7 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <fstream>
 #include <iostream>
-#include <mutex>
 #include <set>
 #include <string>
 #include <thread>
