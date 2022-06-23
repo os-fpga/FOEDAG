8858dbda973d9350c3bf06f35b8e4031272b1aa3
Alain Dargelas <alain.dargelas@gmail.com>
2022-06-22 18:00:35 -0700

    IP generation wip

--- src/IPGenerate/IPCatalog.h
+++ src/IPGenerate/IPCatalog.h
@@ -38,7 +38,6 @@ class Value {
   virtual ~Value() {}
   virtual uint32_t GetValue() = 0;
   virtual const std::string GetSValue() = 0;
-  virtual void SetValue(uint32_t value) = 0;
   virtual const std::string& Name() = 0;
 };
 
