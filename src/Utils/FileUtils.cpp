0abf17c1da3a718769e6f4ef9b2164a9fa669ea4
Alain Dargelas <alain.dargelas@gmail.com>
2022-06-29 18:31:06 -0700

    Utils lib

---
+++ src/Utils/FileUtils.cpp
@@ -0,0 +1,144 @@
+/*
+Copyright 2021 The Foedag team
+
+GPL License
+
+Copyright (c) 2021 The Open-Source FPGA Foundation
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <errno.h>
+#include <limits.h> /* PATH_MAX */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <filesystem>
+ 
+#include "Utils/FileUtils.h"
+
+namespace FOEDAG {
+
+namespace fs = std::filesystem;
+
+bool FileUtils::fileExists(const fs::path& name) {
+  std::error_code ec;
+  return fs::exists(name, ec);
+}
+
+uint64_t FileUtils::fileSize(const fs::path& name) {
+  std::error_code ec;
+  return fs::file_size(name, ec);
+}
+
+bool FileUtils::fileIsDirectory(const fs::path& name) {
+  return fs::is_directory(name);
+}
+
+bool FileUtils::fileIsRegular(const fs::path& name) {
+  return fs::is_regular_file(name);
+}
+
+bool FileUtils::mkDirs(const fs::path& path) {
+  // CAUTION: There is a known bug in VC compiler where a trailing
+  // slash in the path will cause a false return from a call to
+  // fs::create_directories.
+  std::error_code err;
+  fs::create_directories(path, err);
+  return fs::is_directory(path);
+}
+
+bool FileUtils::rmDirRecursively(const fs::path& path) {
+  static constexpr uintmax_t kErrorCondition = static_cast<std::uintmax_t>(-1);
+  std::error_code err;
+  return fs::remove_all(path, err) != kErrorCondition;
+}
+
+fs::path FileUtils::getFullPath(const fs::path& path) {
+  std::error_code ec;
+  fs::path fullPath = fs::canonical(path, ec);
+  return ec ? path : fullPath;
+}
+
+bool FileUtils::getFullPath(const fs::path& path, fs::path* result) {
+  std::error_code ec;
+  fs::path fullPath = fs::canonical(path, ec);
+  bool found = (!ec && fileIsRegular(fullPath));
+  if (result != nullptr) {
+    *result = found ? fullPath : path;
+  }
+  return found;
+}
+
+std::string FileUtils::getFileContent(const fs::path& filename) {
+  std::ifstream in(filename, std::ios::in | std::ios::binary);
+  std::string result;
+
+  if (in) {
+    std::error_code err;
+    const size_t prealloc = fs::file_size(filename, err);
+    if (err.value() == 0) result.reserve(prealloc);
+
+    char buffer[4096];
+    while (in.good() && !in.eof()) {
+      in.read(buffer, sizeof(buffer));
+      result.append(buffer, in.gcount());
+    }
+  } else {
+    result = "FAILED_TO_LOAD_CONTENT";
+  }
+  return result;
+}
+
+fs::path FileUtils::getPathName(const fs::path& path) {
+  return path.has_parent_path() ? path.parent_path() : "";
+}
+
+fs::path FileUtils::basename(const fs::path& path) { return path.filename(); }
+
+fs::path FileUtils::getPreferredPath(const fs::path& path) {
+  return fs::path(path).make_preferred();
+}
+
+std::filesystem::path FileUtils::locateExecFile(const std::filesystem::path& path) {
+  std::filesystem::path result;
+  char *envpath = getenv( "PATH" );
+  char *dir = nullptr;
+
+  for (dir = strtok(envpath, ":"); dir; dir = strtok(NULL, ":")) {
+    fs::path a_path = std::string(dir) / path;
+    if (FileUtils::fileExists(a_path)) {
+      return a_path;
+    }
+  }
+
+  for (fs::path dir : {"/usr/bin", "/usr/local/bin", "~/.local/bin", "./"}) {
+    fs::path a_path = std::string(dir) / path;
+      if (FileUtils::fileExists(a_path)) {
+        return a_path;
+    }
+  }
+
+  return result;
+}
+
+}  // namespace SURELOG
---
+++ src/Utils/StringUtils.h
@@ -0,0 +1,108 @@
+/*
+Copyright 2021 The Foedag team
+
+GPL License
+
+Copyright (c) 2021 The Open-Source FPGA Foundation
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef FOEDAG_STRINGUTILS_H
+#define FOEDAG_STRINGUTILS_H
+#pragma once
+
+#include <map>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <sstream>
+
+namespace FOEDAG {
+
+class StringUtils final {
+ public:
+ 
+  // Modify string string, remove whitespace at the beginning of the string.
+  static std::string& ltrim(std::string& str);
+
+  // Modify string string, remove whitespace at the end of the string.
+  static std::string& rtrim(std::string& str);
+
+  // Modify string, removing spaces on both ends.
+  static std::string& trim(std::string& str);
+
+  // Erase left of the string until given character is reached. If this
+  // is not reached, the string is unchanged. Modifies string.
+  // TODO: this name is confusing, as it does not do the same as the other
+  // trim functions (which trim characters until there is none)
+  static std::string& ltrim(std::string& str, char c);
+
+  // Erase right of the string until given character is reached. If this
+  // is not reached, the string is unchanged. Modifies string.
+  // TODO: this name is confusing, as it does not do the same as the other
+  // trim functions (which trim characters until there is none)
+  static std::string& rtrim(std::string& str, char c);
+
+  // Trim and modify string at assignment character.
+  static std::string& rtrimEqual(std::string& str);
+
+  // Return the last element of a dot-separated path foo.bar.baz -> baz
+  static std::string_view leaf(std::string_view str);
+
+  // In given string "str", replace all occurences of "from" with "to"
+  static std::string replaceAll(std::string_view str, std::string_view from,
+                                std::string_view to);
+
+  // Given a large input, return the content of line number "line".
+  // Lines are 1 indexed.
+  static std::string_view getLineInString(std::string_view text, int line);
+
+  // Split input text into lines at '\n'. This separator is included in the
+  // returned lines; the last line in text might not have a newline so might
+  // not be included.
+  static std::vector<std::string_view> splitLines(std::string_view text);
+
+  // Convert double number with given amount of precision.
+  static std::string to_string(double a_value, const int n = 3);
+
+  // Remove '//' and '#'-style end-of-line comments
+  static std::string removeComments(std::string_view text);
+
+  // Expand environment variables in the form of ${FOO} or $FOO/
+  // (variable followed by slash) in string. Modifies the string.
+  static void autoExpandEnvironmentVariables(std::string* text);
+
+  // Like autoExpandEnvironmentVariables(), but returns modified string.
+  static std::string evaluateEnvVars(std::string_view text);
+
+  static void registerEnvVar(std::string_view var, std::string_view value) {
+    envVars.insert(std::make_pair(var, value));
+  }
+
+  static std::string unquoted(const std::string& text);
+
+ private:
+  StringUtils() = delete;
+  StringUtils(const StringUtils& orig) = delete;
+  ~StringUtils() = delete;
+
+  static std::map<std::string, std::string> envVars;
+
+ private:
+};
+
+};  // namespace FOEDAG
+
+#endif /* FOEDAG_STRINGUTILS_H */
