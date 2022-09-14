7699cfc204dd83567e8c99ff45d9aab6fe6c89f2
ravic-rs <108307503+ravic-rs@users.noreply.github.com>
2022-09-14 15:10:06 -0700

    Remove relative path in settings test file (#651)
    * Remove relative path in settings test file
    
    * Incremented patch version
    
    Co-authored-by: ravic-rs <ravic-rs@users.noreply.github.com>

--- tests/unittest/Settings/Settings_test.cpp
+++ tests/unittest/Settings/Settings_test.cpp
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "../../src/Main/Settings.h"
+#include "Main/Settings.h"
 
 #include <QFile>
 #include <QStringList>
