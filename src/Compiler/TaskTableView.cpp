e76743d1c8e6c72e1e17e88c1fb7be5cbeb562bc
Taras <taras.sydor@rapidsilicon.com>
2022-09-15 17:22:16 +0300

    remove needless include

--- src/Compiler/TaskTableView.cpp
+++ src/Compiler/TaskTableView.cpp
@@ -26,7 +26,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QHeaderView>
 #include <QMenu>
 #include <QPushButton>
-#include <QScrollBar>
 
 #include "TaskManager.h"
 
