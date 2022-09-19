f537e8b9d3c25d00b5edaf7c6f8dc32fb5675e77
Skyler-rs <skyler@rapidsilicon.com>
2022-09-19 09:39:32 -0700

    update generators to use json module

--- tests/Testcases/IPGenerate/IP_Catalog/dummy_generators/RapidSilicon/IP/axis_converter/V1_1/axis_converter_gen.py
+++ tests/Testcases/IPGenerate/IP_Catalog/dummy_generators/RapidSilicon/IP/axis_converter/V1_1/axis_converter_gen.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
+import json
 
 # The following is the captured output of `python3 axis_converter_gen.py --json-template`
 # Which is what the IPCatalog calls when first reading in IPs
 # Foedag currently doesn't have the dependencies(litex/migen) to run that command
 # on the fly so this file is a temporary solution
-print('''{
+
+obj = {
     "core_in_width": 128,
     "core_out_width": 64,
     "core_user_width": 0,
-    "core_reverse": false,
-    "build": false,
+    "core_reverse": False,
+    "build": False,
     "build_dir": "build",
     "build_name": "axis_converter_128b_to_64b",
-    "json": null,
-    "json_template": true
-}''')
+    "json": None,
+    "json_template": True
+}
+
+print( json.dumps(obj) )
