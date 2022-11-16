d47cb22dc8d57daedb8e55945cd5c41b8d2a0b73
coolbreeze413 <coolbreeze413@yahoo.co.in>
2022-11-16 14:44:22 +0530

    add runtime detection and configuration for Tabby CAD

--- src/Compiler/CompilerOpenFPGA_ql.cpp
+++ src/Compiler/CompilerOpenFPGA_ql.cpp
@@ -2440,10 +2440,32 @@ bool CompilerOpenFPGA_ql::Synthesize() {
 #endif // #ifdef _WIN32
 
 #if(AURORA_USE_TABBYCAD == 1)
-  yosysScript = ReplaceAll(yosysScript, "${QL_SYNTH_PASS_NAME}", std::string("synth_qlf"));
-#else // #if(AURORA_USE_TABBYCAD == TRUE)
+  // check if the env variable: YOSYSHQ_LICENSE is defined
+  bool use_tabbycad_for_synthesis = false;
+  const char* const yosyshq_license = std::getenv("YOSYSHQ_LICENSE");
+  if (yosyshq_license != nullptr) {
+    std::filesystem::path path_yosyshq_license = std::string(yosyshq_license);
+    if(FileUtils::FileExists(path_yosyshq_license)) {
+      // if the env variable is defined, and license file path is ok, use Tabby CAD:
+      use_tabbycad_for_synthesis = true;
+    }
+    else {
+      // if license file path is not ok, fallback to open source YosysHQ yosys.
+      Message("YOSYSHQ_LICENSE is set, but license file path is invalid: " + path_yosyshq_license.string());
+      Message("Using open source YosysHQ yosys instead!");
+    }
+  }
+  // if we want to use Tabby CAD, use 'synth_qlf' for the pass_name:
+  if(use_tabbycad_for_synthesis) {
+    yosysScript = ReplaceAll(yosysScript, "${QL_SYNTH_PASS_NAME}", std::string("synth_qlf"));
+  }
+  else {
+    // if we want to use the open source YosysHQ yosys + plugins, use 'synth_quicklogic' for the pass_name:
+    yosysScript = ReplaceAll(yosysScript, "${QL_SYNTH_PASS_NAME}", std::string("synth_quicklogic"));
+  }
+#else // #if(AURORA_USE_TABBYCAD == 1)
   yosysScript = ReplaceAll(yosysScript, "${QL_SYNTH_PASS_NAME}", std::string("synth_quicklogic"));
-#endif // #if(AURORA_USE_TABBYCAD == TRUE)
+#endif // #if(AURORA_USE_TABBYCAD == 1)
 
   yosysScript = ReplaceAll(yosysScript, "${TOP_MODULE}",
                            ProjManager()->DesignTopModule());
@@ -2565,8 +2587,21 @@ bool CompilerOpenFPGA_ql::Synthesize() {
     return false;
   }
 #endif // #if UPSTREAM_UNUSED
+
+
+  std::filesystem::path yosys_executable_path = m_yosysExecutablePath;
+#if(AURORA_USE_TABBYCAD == 1)
+  if(use_tabbycad_for_synthesis) {
+    yosys_executable_path = GetSession()->Context()->BinaryPath().parent_path() /
+                            ".." /
+                            "tabby" /
+                            "bin" /
+                            "yosys";
+  }
+#endif // #if(AURORA_USE_TABBYCAD == 1)
+
   std::string command =
-      m_yosysExecutablePath.string() + " -s " +
+      yosys_executable_path.string() + " -s " +
       std::string(ProjManager()->projectName() + ".ys -l " +
                   ProjManager()->projectName() + "_synth.log");
   (*m_out) << "Synthesis command: " << command << std::endl;
