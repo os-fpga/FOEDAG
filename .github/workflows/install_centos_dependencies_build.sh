9c160c04639c5b4cce6ea93ddcb47d385933a6cf
ravic-rs <108307503+ravic-rs@users.noreply.github.com>
2022-12-01 09:02:42 -0800

    Fix typo in centos dependencies

--- .github/workflows/install_centos_dependencies_build.sh
+++ .github/workflows/install_centos_dependencies_build.sh
@@ -5,7 +5,7 @@ yum install -y epel-release
 curl -C - -O https://cmake.org/files/v3.15/cmake-3.15.7-Linux-x86_64.tar.gz
 tar xzf cmake-3.15.7-Linux-x86_64.tar.gz
 ln -s $PWD/cmake-3.15.7-Linux-x86_64/bin/cmake /usr/bin/cmake
-yum install -y centos-release-rh
+yum install -y centos-release-scl-rh
 yum install -y devtoolset-11
 yum install -y devtoolset-11-toolchain
 yum install -y devtoolset-11-gcc-c++
