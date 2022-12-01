266281be18ab94cc72daaaa93aeb2691eaa12ce7
Volodymyr Kochyn <volodymyr.kochyn@rapidsilicon.com>
2022-12-02 00:14:37 +0200

    Fixed build

--- src/Compiler/TaskModel.cpp
+++ src/Compiler/TaskModel.cpp
@@ -23,6 +23,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <QDebug>
 #include <QIcon>
 
+#include "CompilerDefines.h"
 #include "TaskGlobal.h"
 #include "TaskManager.h"
 
