c1f27cc783384b8fd71049867a26c7ad37b98974
Volodymyr Kochyn <volodymyr.kochyn@rapidsilicon.com>
2022-12-27 14:59:42 +0200

    Add unit tests

--- src/Simulation/Simulator.h
+++ src/Simulation/Simulator.h
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef SIMULATOR_H
 #define SIMULATOR_H
 
+#include <filesystem>
 #include <iostream>
 #include <map>
 #include <string>
