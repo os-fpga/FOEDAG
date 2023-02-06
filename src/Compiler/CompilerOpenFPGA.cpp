61b528b8b3fd86644cafa82c2a93fa790e4bfe2f
behzadmehmood-rs <behzad.mahmood@rapidsilicon.com>
2023-02-06 16:51:55 +0500

    Updating Code Formatting

--- src/Compiler/CompilerOpenFPGA.cpp
+++ src/Compiler/CompilerOpenFPGA.cpp
@@ -195,8 +195,7 @@ void CompilerOpenFPGA::Help(std::ostream* out) {
          << std::endl;
   (*out) << "   synth_options <option list>: Yosys Options" << std::endl;
   (*out) << "   pnr_options <option list>  : VPR Options" << std::endl;
-  (*out)
-      << "   pnr_netlist_lang <blif, eblif, edif, verilog, vhdl> : Chooses vpr input "
+  (*out) << "   pnr_netlist_lang <blif, eblif, edif, verilog, vhdl> : Chooses vpr input "
          "netlist format"
       << std::endl;
   (*out) << "   packing ?clean?            : Packing" << std::endl;
@@ -1848,7 +1847,7 @@ bool CompilerOpenFPGA::Placement() {
   }
 
   std::string netlistFile = ProjManager()->projectName() + "_post_synth.blif";
-  if(GetNetlistType() == NetlistType::EBlif) {
+  if (GetNetlistType() == NetlistType::EBlif) {
     netlistFile = ProjManager()->projectName() + "_post_synth.eblif";
   }
 
