d452d73dc8fb2d39b677287e525aaf74d0779dd0
behzadmehmood-rs <behzad.mahmood@rapidsilicon.com>
2023-02-08 16:28:11 +0500

    Updating CompilerOpenFPGA.cpp

--- src/Compiler/CompilerOpenFPGA.cpp
+++ src/Compiler/CompilerOpenFPGA.cpp
@@ -195,10 +195,10 @@ void CompilerOpenFPGA::Help(std::ostream* out) {
          << std::endl;
   (*out) << "   synth_options <option list>: Yosys Options" << std::endl;
   (*out) << "   pnr_options <option list>  : VPR Options" << std::endl;
-  (*out) << "   pnr_netlist_lang <blif, eblif, edif, verilog, vhdl> : Chooses "
-            "vpr input "
-            "netlist format"
-         << std::endl;
+  (*out)
+      << "   pnr_netlist_lang <blif, edif, verilog, vhdl> : Chooses vpr input "
+         "netlist format"
+      << std::endl;
   (*out) << "   packing ?clean?            : Packing" << std::endl;
   // (*out) << "   global_placement ?clean?   : Analytical placer" << std::endl;
   (*out) << "   place ?clean?              : Detailed placer" << std::endl;
