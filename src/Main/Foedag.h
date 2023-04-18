b3c9a223770776b4b7f3ae3dda1cb2f2391ef819
KochynVolodymyr <95262932+KochynVolodymyr@users.noreply.github.com>
2023-04-18 14:18:32 -0700

    Fixed build issue on msvc (#1067)
    * Fixed build issue on msvc
    
    * Incremented patch version
    
    ---------
    
    Co-authored-by: KochynVolodymyr <KochynVolodymyr@users.noreply.github.com>

--- src/Main/Foedag.h
+++ src/Main/Foedag.h
@@ -18,6 +18,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#ifndef FOEDAG_H
+#define FOEDAG_H
+
 #include "Main/Settings.h"
 #include "Main/ToolContext.h"
 #include "MainWindow/Session.h"
@@ -32,9 +35,6 @@ class TextEditor;
 void registerBasicGuiCommands(FOEDAG::Session* session);
 void registerBasicBatchCommands(FOEDAG::Session* session);
 void registerAllFoedagCommands(QWidget* widget, FOEDAG::Session* session);
-
-#ifndef FOEDAG_H
-#define FOEDAG_H
 
 namespace FOEDAG {
 
