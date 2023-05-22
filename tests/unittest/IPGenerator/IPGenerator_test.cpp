42dc963e6639c002eacec159365efbb20af9d47d
Volodymyr Kochyn <volodymyr.kochyn@rapidsilicon.com>
2023-05-22 11:17:28 +0300

    Add unit tests

--- tests/unittest/IPGenerator/IPGenerator_test.cpp
+++ tests/unittest/IPGenerator/IPGenerator_test.cpp
@@ -22,6 +22,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "IPGenerate/IPGenerator.h"
 
 #include "Compiler/Compiler.h"
+#include "NewProject/ProjectManager/project_manager.h"
+#include "ProjNavigator/tcl_command_integration.h"
 #include "gtest/gtest.h"
 
 namespace FOEDAG {
@@ -90,6 +92,61 @@ TEST(IPGenerate, IpInstanceDupes) {
   ipGen->AddIPInstance(instance4);
   EXPECT_EQ(ipGen->IPInstances().size(), 2)
       << "Ensure the IPInstances count is now 2 ";
+}
+
+TEST(IPGenerate, CheckAllIPPath) {
+  IPCatalog* ipCat = new IPCatalog();
+  Compiler* compiler = new Compiler();
+  IPGenerator* ipGen = new IPGenerator(ipCat, compiler);
+  ProjectManager* pm = new ProjectManager{};
+  Project::Instance()->setProjectName("testProject");
+  compiler->setGuiTclSync(new TclCommandIntegration{pm, nullptr});
+
+  std::vector<Connector*> connections;
+  std::vector<Value*> parameters;
+  Constant* lrange = new Constant(0);
+  Parameter* rrange = new Parameter("Width", 0);
+  Range range(lrange, rrange);
+  Port* port = new Port("clk", Port::Direction::Input, Port::Function::Clock,
+                        Port::Polarity::High, range);
+  connections.push_back(port);
+  IPDefinition* def = new IPDefinition(IPDefinition::IPType::Other, "MOCK_IP",
+                                       "MOCK_IP_wrapper", "path_to_nowhere",
+                                       connections, parameters);
+
+  ipCat->addIP(def);
+
+  EXPECT_EQ(ipGen->IPInstances().size(), 0)
+      << "Ensure the IPInstances is empty so far";
+
+  std::vector<SParameter> params;
+  std::vector<SParameter> params2;
+
+  // Add an ip instance
+  IPInstance* instance = new IPInstance("duplicateName", "version_num", def,
+                                        params, "module_name", "out_file");
+  ipGen->AddIPInstance(instance);
+
+  EXPECT_EQ(ipGen->GetBuildDir(instance),
+            "testProject.IPs/path_to_nowhere/module_name");
+
+  EXPECT_EQ(ipGen->GetSimDir(instance),
+            "testProject.IPs/path_to_nowhere/module_name/sim");
+
+  EXPECT_EQ(ipGen->GetSimArtifactsDir(instance),
+            "testProject.IPs/simulation/path_to_nowhere/module_name");
+
+  EXPECT_EQ(
+      ipGen->GetCachePath(instance),
+      "testProject.IPs/path_to_nowhere/module_name/MOCK_IP_module_name.json");
+
+  EXPECT_EQ(ipGen->GetTmpCachePath(instance),
+            "testProject.IPs/.tmp/path_to_nowhere/module_name/"
+            "MOCK_IP_module_name.json");
+
+  EXPECT_EQ(ipGen->GetTmpPath(), "testProject.IPs/.tmp");
+
+  EXPECT_EQ(ipGen->GetProjectIPsPath(), "testProject.IPs");
 }
 
 }  // namespace FOEDAG
