b67dc94233f116ad685fc154fe6d73c1327e1748
behzadmehmood-rs <behzad.mahmood@rapidsilicon.com>
2023-06-15 16:19:42 +0500

    Updating code

--- src/Compiler/CompilerOpenFPGA.cpp
+++ src/Compiler/CompilerOpenFPGA.cpp
@@ -1961,7 +1961,7 @@ bool CompilerOpenFPGA::Placement() {
       std::string base_name = path.stem().string();
       pincommand += " --port_info ";
       pincommand += (std::filesystem::path(ProjManager()->projectPath()) /
-                     std::string(base_name + "_ports.json"));
+                     std::string(base_name + "_ports.json")).string();
     } else {
       pincommand += " --blif " + netlistFile;
     }
