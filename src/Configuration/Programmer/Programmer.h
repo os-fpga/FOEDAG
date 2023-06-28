24d4b843b3efd7c6bbe0f19c1fe893ec3e163fd1
Guan Yung <guan.yung@rapidsilicon.com>
2023-06-28 10:19:09 +0800

    Fake Programmer APIs implementation

--- src/Configuration/Programmer/Programmer.h
+++ src/Configuration/Programmer/Programmer.h
@@ -22,6 +22,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef PROGRAMMER_H
 #define PROGRAMMER_H
 
+#include <atomic>
+#include <functional>
+#include <regex>
+
 #include "CFGCommon/CFGArg_auto.h"
 #include "CFGCommon/CFGCommon.h"
 
@@ -33,6 +37,35 @@ struct ProgrammerCommand {
   std::string executable_cmd;
   bool is_error = false;
 };
+
+struct Device {
+  int index;
+  std::string name;
+  std::string jtagId;
+  unsigned int mask;
+  unsigned int irlength;
+  int flashSize;
+};
+
+enum class Operation { Erase = 1, BlankCheck = 2, Program = 4, Verify = 8 };
+
+struct CfgStatus {
+  bool cfgDone;
+  bool cfgError;
+};
+
+inline Operation operator|(Operation lhs, Operation rhs) {
+  using T = std::underlying_type_t<Operation>;
+  return static_cast<Operation>(static_cast<T>(lhs) | static_cast<T>(rhs));
+}
+
+inline Operation operator&(Operation lhs, Operation rhs) {
+  using T = std::underlying_type_t<Operation>;
+  return static_cast<Operation>(static_cast<T>(lhs) & static_cast<T>(rhs));
+}
+
+using ProgressCallback = std::function<void(double)>;
+using OutputCallback = std::function<void(std::string)>;
 
 std::string buildFpgaProgramCommand(const std::string& bitstream_file,
                                     const std::string& config_file,
@@ -52,4 +85,15 @@ bool isOperationRequested(const std::string& operation,
                           const std::vector<std::string>& supportedOperations);
 void programmer_entry(const CFGCommon_ARG* cmdarg);
 
+// Backend API
+bool ListDevices(std::vector<Device>& devices, std::string& outputMsg);
+bool GetFpgaStatus(const Device& device, CfgStatus& status);
+int ProgramFpga(const Device& device, const std::string& bitfile,
+                const std::string& cfgfile, std::ostream* outStream,
+                OutputCallback callbackMsg, ProgressCallback callbackProgress,
+                std::atomic<bool> stop);
+bool ProgramFlash(const Device& device, const std::string& bitfile,
+                  const std::string& cfgfile, Operation modes,
+                  std::ostream* outStream, OutputCallback callbackMsg,
+                  ProgressCallback callbackProgress, std::atomic<bool> stop);
 #endif
