d0198d625dd29b86cb6b9cc5d3b845a29bb83330
Volodymyr Kochyn <volodymyr.kochyn@rapidsilicon.com>
2023-07-18 23:10:06 +0300

    Initial timing report

--- src/Compiler/Reports/TimingAnalysisReportManager.cpp
+++ src/Compiler/Reports/TimingAnalysisReportManager.cpp
@@ -30,6 +30,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "DefaultTaskReport.h"
 #include "TableReport.h"
 #include "Utils/FileUtils.h"
+#include "qdebug.h"
 
 #ifndef PRODUCTION_BUILD
 #define TIMING_SUMMARY
