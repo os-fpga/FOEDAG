d3e12343049cd55ca1f854b7b179282f7c6dd2a9
Guan Yung <guan.yung@rapidsilicon.com>
2023-07-24 10:20:56 +0800

    Install libusb compact dependencies

--- .github/workflows/install_macos_dependencies_build.sh
+++ .github/workflows/install_macos_dependencies_build.sh
@@ -1,5 +1,5 @@
 # Install required dependencies for Mac OS systems
-brew install qt5 libusb
+brew install qt5 libusb libusb-compat
 export PATH="/usr/local/opt/qt@5/bin:$PATH"
 export LDFLAGS="-L/usr/local/opt/qt@5/lib"
 export CPPFLAGS="-I/usr/local/opt/qt@5/include"
