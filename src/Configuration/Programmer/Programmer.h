13d446af1e866a43c3ef71365105d12dba394e78
Guan Yung <guan.yung@rapidsilicon.com>
2023-07-25 16:42:42 +0800

    Fix missing map header

--- src/Configuration/Programmer/Programmer.h
+++ src/Configuration/Programmer/Programmer.h
@@ -25,6 +25,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <functional>
 #include <map>
 #include <tuple>
+#include <map>
 
 #include "CFGCommon/CFGArg_auto.h"
 #include "CFGCommon/CFGCommon.h"
