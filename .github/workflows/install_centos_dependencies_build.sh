144f5dcc468ad05db0590f555120b613e83aa95c
Guan Yung <guan.yung@rapidsilicon.com>
2023-08-15 11:20:40 +0800

    Add libusbx-devel to centos dependencies build

--- .github/workflows/install_centos_dependencies_build.sh
+++ .github/workflows/install_centos_dependencies_build.sh
@@ -24,7 +24,7 @@ yum install -y mesa-libGL-devel
 yum install -y libxcb libxcb-devel xcb-util xcb-util-devel libxkbcommon-devel libxkbcommon-x11-devel
 yum install -y xcb-util-image-devel xcb-util-keysyms-devel xcb-util-renderutil-devel xcb-util-wm-devel
 yum install -y gtk3-devel zip unzip
-yum install -y libusb-devel
+yum install -y libusbx-devel libusb-devel
 yum install -y pkgconfig
 ln -s $PWD/cmake-3.15.7-Linux-x86_64/bin/ctest /usr/bin/ctest
 echo 'QMAKE_CC=/opt/rh/devtoolset-11/root/usr/bin/gcc' >> $GITHUB_ENV
