00f2b400a42fc0af4ca32e8fdc654d636a2ccc4e
Guan Yung <guan.yung@rapidsilicon.com>
2023-08-22 10:58:20 +0800

    Fixed include StringUtils header

--- src/Configuration/Programmer/Programmer_helper.cpp
+++ src/Configuration/Programmer/Programmer_helper.cpp
@@ -25,8 +25,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <unordered_set>
 
 #include "Programmer.h"
+#include "Utils/StringUtils.h"
 #include "libusb.h"
-#include "src/Utils/StringUtils.h"
 
 namespace FOEDAG {
 
