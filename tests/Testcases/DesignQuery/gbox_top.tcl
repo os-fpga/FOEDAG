0c77479f3873b7b909634c35289f8578e94b1350
Zaid Tahir <zaid.butt.tahir@gmail.com>
2023-08-31 14:06:16 -0400

    Disabled get_file_ids test case temporarily

--- tests/Testcases/DesignQuery/gbox_top.tcl
+++ tests/Testcases/DesignQuery/gbox_top.tcl
@@ -1,6 +1,5 @@
 # puts  [ test_device_modeling_tcl ]
 # puts [ exec date ]
-# puts [ get_file_ids ]  # works now after adding additional code
 
 
 # use "verbose" with tcl command to display debugging information for the sdt generating cpp library "sdtgen_cpp_nlohman_lib_v6"
@@ -17,5 +16,10 @@ puts [ sdt_gen_soc_node ]
 puts [ sdt_gen_root_metadata_node ]
 # puts [ sdt_gen_system_device_tree verbose ]
 puts [ sdt_gen_system_device_tree ]
+
+# putting this tcl command "get_file_ids" before the sdt tcl commands above gives out some sort of parse error TODO: Fix this
+puts [ get_file_ids ]
+
+
 
 # run this tcl script by running this command in FOEDAG folder: ./build/bin/foedag --batch --script ./tests/Testcases/DesignQuery/gbox_top.tcl
