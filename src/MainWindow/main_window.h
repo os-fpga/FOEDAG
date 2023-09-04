f2d39b44f504e5ace60739ecf0fda40236945df9
Volodymyr Kochyn <volodymyr.kochyn@rapidsilicon.com>
2023-09-04 16:44:06 +0300

    Open IP configuration when user select IP Reconfiguration action from context menu

--- src/MainWindow/main_window.h
+++ src/MainWindow/main_window.h
@@ -117,7 +117,9 @@ class MainWindow : public QMainWindow, public TopLevelInterface {
  public slots:
   void updateSourceTree();
   void handleIpTreeSelectionChanged();
-  void openIpConfigurationDialog();
+  void openIpConfigurationDialog(const QString& ipName = {},
+                                 const QString& moduleName = {},
+                                 const QStringList& paramList = {});
   void handleIpReConfigRequested(const QString& ipName,
                                  const QString& moduleName,
                                  const QStringList& paramList);
