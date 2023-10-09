1484da8574fa6f0a07b5dc802417d7ff14f67262
Guan Yung <guan.yung@rapidsilicon.com>
2023-10-09 15:22:59 +0800

    Remove stale programmer configuration code

--- src/Configuration/CFGCompiler/CFGCompiler.cpp
+++ src/Configuration/CFGCompiler/CFGCompiler.cpp
@@ -48,7 +48,6 @@ static bool programmer_flow(CFGCompiler* cfgcompiler, int argc,
   status = arg->parse(argc - 1, argvPtr, &errors);
   cfgcompiler->m_cmdarg.arg = arg;
   cfgcompiler->m_cmdarg.toolPath = compiler->GetProgrammerToolExecPath();
-  cfgcompiler->m_cmdarg.searchPath = compiler->GetConfigFileSearchDirectory();
   return status;
 }
 
@@ -129,7 +128,6 @@ int CFGCompiler::Compile(CFGCompiler* cfgcompiler, bool batchMode) {
       compiler->FilePath(Compiler::Action::Analyze).string();
   cfgcompiler->m_cmdarg.synthesisPath =
       compiler->FilePath(Compiler::Action::Synthesis).string();
-  cfgcompiler->m_cmdarg.searchPath = compiler->GetConfigFileSearchDirectory();
   cfgcompiler->m_cmdarg.binPath = compiler->GetBinPath().string();
 
   // Call Compile()
