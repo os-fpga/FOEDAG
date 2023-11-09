bb207c75efb9cfceed2970ab0bb20514db87eae9
Oleksandr <apivovarov@quicklogic.com>
2023-11-09 17:03:03 +0200

    sequential_map::push_back removes previosly added values with same key, otherwise the methods sequential_map::operator[] and sequential_map::value() will return value for the first(oldest) occured key

--- src/Utils/sequential_map.h
+++ src/Utils/sequential_map.h
@@ -49,7 +49,20 @@ class sequential_map {
     return defaultValue;
   }
 
-  void push_back(const std::pair<Key, Value> &p) { m_data.push_back(p); }
+  void push_back(const std::pair<Key, Value> &p) 
+  { 
+    // remove previosly added pair with same key, to escape sitation of having multiple keys
+    for (auto it=m_data.begin(); it != m_data.end();) {
+      if (it->first == p.first) {
+        it = m_data.erase(it);
+      } else {
+        ++it;
+      }
+    }
+    //
+
+    m_data.push_back(p); 
+  }
 
   size_t count() const { return m_data.size(); }
 
