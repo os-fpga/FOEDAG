4f5c8068a0e6d6e766f6d5d33c01b6e69266f5e0
Oleksandr <apivovarov@quicklogic.com>
2023-11-10 15:46:57 +0200

    fix compile warnings in keys.h

--- src/InteractivePathAnalysis/client/keys.h
+++ src/InteractivePathAnalysis/client/keys.h
@@ -1,11 +1,11 @@
 #pragma once
 
-static const char* KEY_JOB_ID = "JOB_ID";
-static const char* KEY_CMD = "CMD";
-static const char* KEY_OPTIONS = "OPTIONS";
-static const char* KEY_DATA = "DATA";
-static const char* KEY_STATUS = "STATUS";
-static const char* END_TELEGRAM_SEQUENCE = "###___END_FRAME___###";
+constexpr const char* KEY_JOB_ID = "JOB_ID";
+constexpr const char* KEY_CMD = "CMD";
+constexpr const char* KEY_OPTIONS = "OPTIONS";
+constexpr const char* KEY_DATA = "DATA";
+constexpr const char* KEY_STATUS = "STATUS";
+constexpr const char* END_TELEGRAM_SEQUENCE = "###___END_FRAME___###";
 
 enum CMD {
     CMD_GET_PATH_LIST_ID=0,
