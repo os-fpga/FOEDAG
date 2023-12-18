50dfb1d8a0ecdf907bcbb53cc3123b55ec6fca2b
Guan Yung <guan.yung@rapidsilicon.com>
2023-12-18 14:56:03 +0800

    Add programmer error code header

---
+++ src/Configuration/Programmer/Programmer_error_code.h
@@ -0,0 +1,20 @@
+#pragma once
+namespace FOEDAG {
+
+enum ProgrammerErrorCode {
+  NoError = 0,
+  GeneralCmdError = -1,
+  UnknownFirmware = -2,
+  BufferTimeout = -3,
+  CmdTimeout = -4,
+  ConfigError = -5,
+  FsblBootFail = -6,
+  CableNotFound = -7,
+  CableNotSupported = -8,
+  DeviceNotFound = -9,
+  BitfileNotFound = -10,
+  OpenOCDExecutableNotFound = -11,
+  InvalidFlashSize = -12,
+};
+
+}
