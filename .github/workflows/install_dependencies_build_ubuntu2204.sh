bfb846136431fc2977ba0d199479f19f0e2fcdfa
coolbreeze413 <coolbreeze413@yahoo.co.in>
2024-02-06 20:32:21 +0530

    initial winbuild commit for ql

---
+++ .github/workflows/install_dependencies_build_ubuntu2204.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# The package list is designed for Ubuntu 22.04 LTS
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+apt-get update
+apt-get install -y \
+    g++-11 gcc-11 \
+    tclsh \
+    cmake \
+    build-essential \
+    google-perftools \
+    libgoogle-perftools-dev \
+    uuid-dev \
+    lcov \
+    valgrind \
+    xorg \
+    qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools \
+    xvfb \
+    yosys \
+    automake
