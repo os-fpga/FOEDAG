c8f0459b4bfbe9f5dda242dc569bd142b0824d0a
Oleksandr <apivovarov@quicklogic.com>
2024-02-27 14:14:05 +0200

    undo dev code with bypassing EULA check, commited previously by mistake

--- src/MainWindow/main_window_ql.cpp
+++ src/MainWindow/main_window_ql.cpp
@@ -990,7 +990,6 @@ void MainWindow::createActions() {
 }
 
 void MainWindow::showEULADialog() {
-  return;
   // check if user has already accepted all licenses, else
   // create a stacked widget, with a widget for each component license
   // on Accept move to next page
