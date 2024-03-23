52df1940a8cea866ddb11557598fa879f3f4965c
chungshien-chai <chungshien.chai@gmail.com>
2024-03-23 01:41:37 -0700

    Update library

--- .github/workflows/install_ubuntu_dependencies_build.sh
+++ .github/workflows/install_ubuntu_dependencies_build.sh
@@ -16,7 +16,7 @@ sudo apt install -y \
   automake \
   libusb-1.0-0-dev \
   pkg-config \
-  python-dev
+  python3-dev
 
 wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
 sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-jammy.list http://packages.lunarg.com/vulkan/lunarg-vulkan-jammy.list
