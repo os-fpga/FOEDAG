5937c4cc30b53ff29ef993860a062a138e776f9e
chungshien-chai <chungshien.chai@gmail.com>
2024-03-23 02:28:34 -0700

    Misc

--- src/Configuration/CFGCommon/CFGCommon.cpp
+++ src/Configuration/CFGCommon/CFGCommon.cpp
@@ -13,15 +13,18 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+// clang-format off
+#include <Python.h>
+
 #include "CFGCommon.h"
+// clang-format on
 
 #ifdef _WIN32
 #include <Windows.h>
 #else
 #include <unistd.h>
 #endif
-
-#include <Python.h>
 
 #include <algorithm>
 #include <cstdarg>
