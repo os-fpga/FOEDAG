1183a8a80d73c83d99141b8339c64a45dde7489e
KochynVolodymyr <volodymyr.kochyn@rapidsilicon.com>
2024-04-10 23:38:14 +0300

    Fixed MAC and WIN build

--- src/Main/TclSimpleParser.h
+++ src/Main/TclSimpleParser.h
@@ -23,6 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <string>
 #include <vector>
 
+typedef unsigned int uint;
+
 namespace FOEDAG {
 
 class TclSimpleParser {
