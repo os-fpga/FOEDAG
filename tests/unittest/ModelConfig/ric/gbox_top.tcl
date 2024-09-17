0805bfe05d35d43dfb70d8b82f90efce37e2729b
chungshien-chai <156862023+chungshien-chai@users.noreply.github.com>
2024-09-17 12:08:58 -0700

    Set PEER_IS_ON for peer gearbox (#1701)
    * Set PEER_IS_ON for peer gearbox
    
    * Incremented patch version
    
    * Enable PEER_on by default
    
    ---------
    
    Co-authored-by: chungshien-chai <chungshien-chai@users.noreply.github.com>

--- tests/unittest/ModelConfig/ric/gbox_top.tcl
+++ tests/unittest/ModelConfig/ric/gbox_top.tcl
@@ -6,7 +6,7 @@ define_block -name GBOX_TOP
 ####################################
 define_attr -block GBOX_TOP -name RATE         -addr 0  -width 4 -enumname GBOX_ATT_0  -enum {Three 3} {Four 4} {Five 5} {Six 6} {Seven 7} {Eight 8} {Night 9} {Ten 10}
 define_attr -block GBOX_TOP -name MASTER_SLAVE -addr 4  -width 1 -enumname GBOX_ATT_1  -enum {Slave 0} {Master 1}
-define_attr -block GBOX_TOP -name PEER_IS_ON   -addr 5  -width 1 -enumname GBOX_ATT_2  -enum {PEER_off 0} {PEER_on 1}
+define_attr -block GBOX_TOP -name PEER_IS_ON   -addr 5  -width 1 -enumname GBOX_ATT_2  -enum {PEER_off 0} {PEER_on 1} -default PEER_on
 define_attr -block GBOX_TOP -name TX_CLOCK_IO  -addr 6  -width 1 -enumname GBOX_ATT_3  -enum {TX_normal_IO 0} {TX_clock_IO 1}
 define_attr -block GBOX_TOP -name TX_DDR_MODE  -addr 7  -width 2 -enumname GBOX_ATT_4  -enum {TX_direct 0} {TX_ddr 1} {TX_sdr 2}
 define_attr -block GBOX_TOP -name TX_BYPASS    -addr 9  -width 1 -enumname GBOX_ATT_5  -enum {TX_gear_on 0} {TX_bypass 1}
