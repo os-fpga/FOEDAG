c58277b956257c8dd88af369f596eb991cd15e0e
Ravikiran Chollangi <ravikiran.chollangi@gmail.com>
2024-11-08 11:01:41 -0800

    Add power_data resource to device.xml

--- src/Compiler/CompilerOpenFPGA.cpp
+++ src/Compiler/CompilerOpenFPGA.cpp
@@ -4049,7 +4049,10 @@ bool CompilerOpenFPGA::LoadDeviceData(
                 BaseDeviceName(name);
                 // field is used for identify base for custom device
                 // no action so far
-              } else {
+              } else if (file_type == "power_data") {
+                // field will be used for power data config, skip for now.
+              }
+                else {
                 ErrorMessage("Invalid device config type: " + file_type + "\n");
                 status = false;
               }
